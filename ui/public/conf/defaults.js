54797ad941974bca1fcdf406d474404b5c6a720e
Dean Tribble <tribble@e-dean.com>
2020-04-21 11:05:13 -0700

    chore: remove a local conf file from the repository

--- ui/public/conf/defaults.js
+++
@@ -1,9 +0,0 @@
-  // GENERATED FROM /Users/katesills/code/dapp-encouragement/api/deploy.js
-  export default {
-  "INSTANCE_REG_KEY": "encouragementinstance_5496",
-  "CONTRACT_NAME": "encouragement",
-  "INSTALLATION_REG_KEY": "encouragementinstallation_9794",
-  "BRIDGE_URL": "http://127.0.0.1:8000",
-  "API_URL": "http://127.0.0.1:8000"
-};
-  
