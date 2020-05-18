1742780108c065b472b76e431c5ba581ca503b54
Ivan Molto <ivanmolto@gmail.com>
2020-05-19 01:26:55 +0200

    chore: Adapt main to connect with game

--- ui/public/src/main.js
+++ ui/public/src/main.js
@@ -8,116 +8,121 @@ const { INSTANCE_REG_KEY } = dappConstants;
 /**
  * @type {Object.<string, HTMLSelectElement>}
  */
-const selects = {
-  $brands: /** @type {HTMLSelectElement} */ (document.getElementById('brands')),
-  $tipPurse: /** @type {HTMLSelectElement} */ (document.getElementById('tipPurse')),
-};
 
-const $forFree = /** @type {HTMLInputElement} */ (document.getElementById('forFree'));
-const $forTip = /** @type {HTMLInputElement} */ (document.getElementById('forTip'));
-const $encourageForm = /** @type {HTMLFormElement} */ (document.getElementById('encourageForm'));
+// const selects = {
+// $brands: /** @type {HTMLSelectElement} */ (document.getElementById('brands')),
+//$tipPurse: /** @type {HTMLSelectElement} */ (document.getElementById(
+//'tipPurse',
+// )),
+// };
+
+// const $forFree = /** @type {HTMLInputElement} */ (document.getElementById(
+// 'forFree',
+// ));
+const $forTip = /** @type {HTMLInputElement} */ (document.getElementById(
+  'forTip',
+));
+const $encourageForm = /** @type {HTMLFormElement} */ (document.getElementById(
+  'encourageForm',
+));
 
 export default async function main() {
-  selects.$brands.addEventListener('change', () => {
-    flipSelectedBrands(selects);
-  });
-  
   /**
    * @param {{ type: string; data: any; walletURL: string }} obj
    */
   const walletRecv = obj => {
     switch (obj.type) {
       case 'walletUpdatePurses': {
-        const purses = JSON.parse(obj.data);
-        console.log('got purses', purses);
-        walletUpdatePurses(purses, selects);
-        $inputAmount.removeAttribute('disabled');
+        // const purses = JSON.parse(obj.data);
+        // console.log('got purses', purses);
+        // walletUpdatePurses(purses, selects);
+        // $inputAmount.removeAttribute('disabled');
         break;
       }
       case 'walletURL': {
-       // Change the form action to URL.
-       $encourageForm.action = `${obj.walletURL}`;
-       break;
+        // Change the form action to URL.
+        // $encourageForm.action = `${obj.walletURL}`;
+        break;
       }
     }
   };
 
-  const $numEncouragements = /** @type {HTMLInputElement} */ (document.getElementById('numEncouragements'));
-  const $inputAmount = /** @type {HTMLInputElement} */ (document.getElementById('inputAmount'));
+  // const $numEncouragements = /** @type {HTMLInputElement} */ (document.getElementById('numEncouragements'));
+  // const $inputAmount = /** @type {HTMLInputElement} */ (document.getElementById('inputAmount'));
 
   /**
    * @param {{ type: string; data: any; }} obj
    */
   const apiRecv = obj => {
     switch (obj.type) {
-      case 'encouragement/getEncouragementResponse':
-        alert(`Encourager says: ${obj.data}`);
+      case 'voltair/getLootResponse':
+        // alert(`Encourager says: ${obj.data}`);
         break;
-      case 'encouragement/encouragedResponse':
-        $numEncouragements.innerHTML = obj.data.count;
-        break;
+      // case 'encouragement/encouragedResponse':
+      // $numEncouragements.innerHTML = obj.data.count;
+      // break;
     }
   };
 
-  const $encourageMe = /** @type {HTMLInputElement} */ (document.getElementById('encourageMe'));
-  
+  const $purchaseLoot = document.getElementById('purchaseLoot');
+
   const walletSend = await connect('wallet', walletRecv).then(walletSend => {
-    walletSend({ type: 'walletGetPurses'});
+    walletSend({ type: 'walletGetPurses' });
     return walletSend;
   });
 
   const apiSend = await connect('api', apiRecv).then(apiSend => {
     apiSend({
-      type: 'encouragement/subscribeNotifications',
+      type: 'voltair/subscribeNotifications',
     });
 
-    $encourageMe.removeAttribute('disabled');
-    $encourageMe.addEventListener('click', () => {
-      if ($forFree.checked) {
-        $encourageForm.target = '';
-        apiSend({
-          type: 'encouragement/getEncouragement',
-        });
-      }
-      if ($forTip.checked) {
-        $encourageForm.target = '_blank';
-        const now = Date.now();
-        const offer = {
-          // JSONable ID for this offer.  This is scoped to the origin.
-          id: now,
-      
-          // Contract-specific metadata.
-          instanceRegKey: INSTANCE_REG_KEY,
-      
-          // Format is:
-          //   hooks[targetName][hookName] = [hookMethod, ...hookArgs].
-          // Then is called within the wallet as:
-          //   E(target)[hookMethod](...hookArgs)
-          hooks: {
-            publicAPI: {
-              getInvite: ['makeInvite'], // E(publicAPI).makeInvite()
+    $purchaseLoot.addEventListener('click', loots => {
+      apiSend({
+        type: 'voltair/getLoot',
+      });
+
+      const now = Date.now();
+      const offer = {
+        // JSONable ID for this offer.  This is scoped to the origin.
+        id: now,
+
+        // Contract-specific metadata.
+        instanceRegKey: INSTANCE_REG_KEY,
+
+        // Format is:
+        //   hooks[targetName][hookName] = [hookMethod, ...hookArgs].
+        // Then is called within the wallet as:
+        //   E(target)[hookMethod](...hookArgs)
+        hooks: {
+          publicAPI: {
+            getInvite: ['makeBuyerInvite'], // E(publicAPI).makeInvite()
+          },
+        },
+
+        proposalTemplate: {
+          want: {
+            Loot: {
+              pursePetname: 'Game Account',
+              extent: loots,
             },
           },
-      
-          proposalTemplate: {
-            give: {
-              Tip: {
-                // The pursePetname identifies which purse we want to use
-                pursePetname: selects.$tipPurse.value,
-                extent: Number($inputAmount.value),
-              },
+
+          give: {
+            Money: {
+              // The pursePetname identifies which purse we want to use
+              pursePetname: 'Fun budget',
+              extent: loots[0].price,
             },
-            exit: { onDemand: null },
           },
-        };
-        walletSend({
-          type: 'walletAddOffer',
-          data: offer
-        });
-        alert('Please approve your tip, then close the wallet.')
-      }
+          exit: { onDemand: null },
+        },
+      };
+      walletSend({
+        type: 'walletAddOffer',
+        data: offer,
+      });
+      alert('Please approve your tip, then close the wallet.');
     });
-    
     return apiSend;
   });
 }
